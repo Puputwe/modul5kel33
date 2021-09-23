ebe4f39bec11a64d4c39ee359aea0d79a642526a
Puputwe <werdiningsihpuput@gmail.com>
2021-09-23 23:36:40 +0700

    edit

---
+++ src/tekkom.js
@@ -0,0 +1,66 @@
+import React, {Component} from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+
+export default class tekkom extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            tekkom:[],
+            visible: false,
+        }
+    }
+
+    handleButton = (alamat) => {
+        alert("Alamat: "+alamat)
+    }
+    
+    componentDidMount(){
+        axios({
+            method: "get",
+            url: "http://localhost:3000/data",
+            headers: {
+                accept: "*/*"
+            }
+        }).then((data)=>{
+            console.log(data.data);
+            this.setState({
+                tekkom:data.data
+            })
+        }).catch((error)=>{console.log(error)})
+    }
+
+    render() {
+        return (
+            <div style={{ marginTop: 20 }}>
+                <AppBar style={{ padding: "10px"}} >
+                    <Typography style={{ margin: "auto" }}>List Nama</Typography>
+                </AppBar>
+                <Grid container md={10} spacing={5} style={{ marginTop:"100px", marginLeft:"auto", marginRight:"auto"}}>
+                    {this.state.tekkom.map((results, index) => {
+                        return (
+                            <Grid item key={results.name} md={3} >
+                                <Card>
+                                    <CardActionArea onClick={() => this.handleButton(results.email)}>
+                                        <CardContent style={{ backgroundColor: '#cfe8fc'}}>
+                                            <Typography>Name : {results.name}</Typography>
+                                            <Typography>Username : {results.username}</Typography>
+                                            <Typography>Email : {results.email}</Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </Grid>
+                        );
+                    })}
+                </Grid>
+            </div>
+        );
+       
+    }
+       
+}
